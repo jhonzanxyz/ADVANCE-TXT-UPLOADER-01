1965794549f383fe915a28874dff3574464fc4ee
jhonzanxyz <jhonzanxyz@gmail.com>
2025-05-01 12:24:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -448,7 +448,7 @@ async def help_command(client: Client, msg: Message):
     await msg.reply_text(help_text)
 
 # Upload command handler
-@bot.on_message(filters.command(["chahal"]))
+@bot.on_message(filters.command(["drm"]))
 async def upload(bot: Client, m: Message):
     if not is_authorized(m.chat.id):
         await m.reply_text(ibb)
